71d13492dbfef376aa98b02f0d376dae1ee90bb7
Nicolo T. <nicolotonci@tiscali.it>
2025-01-14 11:05:26 +0000

    Automatized torus test

--- torus/runTorus.sh
+++ torus/runTorus.sh
@@ -2,14 +2,14 @@ RIPETIZIONI=2
 N_TICKS=0
 MACCHINE=(localhost localhost localhost localhost)
 CONFIG_FILENAME=torus_generated.json
-GROUPS_TOT=4 # minimum 2 # NUMBER OF STAGES
+GROUPS_MAX=8 # must be power of 2 # NUMBER OF STAGES
 BATCH_SIZE=1
 BATCH_BYTE_SIZE=2M 
 
 echoerr() { printf "%s\n" "$*" >&2; }
 
 generaFileConfig() {
-    echoerr $1 $2 $3 $4 $5 $6
+    #echoerr $1 $2 $3 $4 $5 $6
 
     local fileName=$1
     local groupsTot=$2
@@ -48,7 +48,6 @@ generaFileConfig() {
 }" >> $fileName
 }
 
-generaFileConfig $CONFIG_FILENAME $GROUPS_TOT MACCHINE[@] $BATCH_SIZE $BATCH_BYTE_SIZE
 
 #generate the list of machines separated by comma
 IFS=','       # Imposta IFS su ","
@@ -61,11 +60,13 @@ echoerr "Machine used: $mpi_machines_list"
 exec 2>/dev/null
 
 printf "Stages;Tasks;Time;Msg/s\n"
-
-for((ripetizione=0; ripetizione<$RIPETIZIONI; ripetizione+=1)); do
-    if [ "$USE_SLURM" -eq 1 ]; then
-        srun -N $GROUPS_TOT --exclusive --cpus-per-task=36 --export=ALL,UCX_ZCOPY_THRESH=2M $(pwd)/torus $GROUPS_TOT $N_TICKS  --DFF_Config=$(pwd)/$CONFIG_FILENAME | tail -1
-    else
-        mpirun -H $mpi_machines_list  -np $GROUPS_TOT $(pwd)/torus $GROUPS_TOT $N_TICKS  --DFF_Config=$(pwd)/$CONFIG_FILENAME | tail -1
-    fi
+for((GROUPS_TOT=2; GROUPS_TOT<$GROUPS_MAX; GROUPS_TOT*=2)); do
+    generaFileConfig $CONFIG_FILENAME $GROUPS_TOT MACCHINE[@] $BATCH_SIZE $BATCH_BYTE_SIZE
+    for((ripetizione=0; ripetizione<$RIPETIZIONI; ripetizione+=1)); do
+        if [ "$USE_SLURM" -eq 1 ]; then
+            srun -N $GROUPS_TOT --exclusive --cpus-per-task=36 --export=ALL,UCX_ZCOPY_THRESH=2M $(pwd)/torus $GROUPS_TOT $N_TICKS  --DFF_Config=$(pwd)/$CONFIG_FILENAME | tail -1
+        else
+            mpirun -H $mpi_machines_list  -np $GROUPS_TOT $(pwd)/torus $GROUPS_TOT $N_TICKS  --DFF_Config=$(pwd)/$CONFIG_FILENAME | tail -1
+        fi
+    done
 done
